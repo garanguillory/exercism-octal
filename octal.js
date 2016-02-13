4cab7d6ed49b06d72ae152c30af13d99c30b4e8c
Garan Guillory <garanguillory@gmail.com>
2016-02-13 14:01:21 -0700

    All tests passing

--- octal.js
+++ octal.js
@@ -1,31 +1,32 @@
-
 var Octal = function(input){
 
 	this.toDecimal = function(){
-		var stringArray = input.split('').reverse();
-		var numberArray = stringArray.map(function(element){
-				return Number(element);
-		});
-		// return numberArray;
-		var sub = 
-			numberArray.map(function(element,index){
-					return element * Math.pow(8,index);
-			}).reduce(function(accum,curr){return accum + curr});
-		return sub;
+		
+		if(Number(input)){
+
+			var stringArray = input.split('').reverse();
+
+				if(stringArray.indexOf('8') == -1){
+
+							var numberArray = stringArray.map(function(element){
+									return Number(element);
+							});
+
+							var answer = 
+								numberArray.map(function(element,index){
+										return element * Math.pow(8,index);
+								}).reduce(function(accum,curr){return accum + curr});
+
+							return answer;
+						
+				} else {
+					return 0;
+				}
+		} else {
+			return 0;
+		}
 	};
 
 };
-
-
-
-
-// So:
-// ```
-//    233 # octal
-//  = 2*8^2 + 3*8^1 + 3*8^0
-//  = 2*64  + 3*8   + 3*1
-//  = 128   + 24    + 3
-//  = 155
-// ```
 
 module.exports = Octal;
